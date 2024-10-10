7f42b36ef131967f4918bb39e6426d9d2050918f
Diego Alex Faria Lustosa <diegolustosasantos@gmail.com>
2024-10-09 22:16:32 -0300

    api mercado
    subindo

---
+++ api_mercado/database.sql
@@ -0,0 +1,26 @@
+--COMANDOS DLL - ESTRUTURA DO BANCO DE DADOS MERCADO --
+--use mercado--
+
+CREATE DATABASE mercado;
+CREATE TABLE mercado.produto (
+    id int not null auto_increment primary key,
+    nome varchar(255) not null,
+    preco decimal(9,2),
+    status boolean default true
+);
+CREATE TABLE mercado.cliente (
+    id int not null auto_increment primary key,
+    nome varchar(255) not null,
+    varchar(255) not null,
+    telefone varchar(25),
+    status boolean default true
+);
+CREATE TABLE mercado.pedido(
+    id int not null auto_increment primary key,
+    id_cliente int not null,
+    foreign key(id_cliente) references cliente(id),
+    id_produto int not null,
+    foreign key(id_produto) references produto(id),
+    qtd int,
+    total decimal(9,2)
+);
