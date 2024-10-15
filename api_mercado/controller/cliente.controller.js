cb96a161e5510f880236293f5d1d7f4806bf841c
Diego Alex Faria Lustosa <diegolustosasantos@gmail.com>
2024-10-14 22:13:43 -0300

    controller update
    modificação na regra de negócio

--- api_mercado/controller/cliente.controller.js
+++ api_mercado/controller/cliente.controller.js
@@ -1,16 +1,42 @@
 const conn = require("../mysql-connection");
+const clientes = [];
+
 module.exports = ({
-    cadastro: (req, res) => { },
-    consultar: (req, res) => {
-        conn.raw("SELECT * FROM CLIENTE").then((data) => {
-            res.send(data[0]);
-    }).catch((erro) => {
-            res.send("Erro ao consultar os clientes!");
+    cadastro: (req, res) => {
+    const {nome, email, telefone} = req.body;
+    var comando = ``;
+    
+    if(!nome || !email || !telefone) {
+        comando = `insert into cliente(nome, telefone) VALUES('${nome}', null)`
+     } else {
+        comando = `insert into cliente(nome, telefone) values('${nome}', ${telefone})`
+     }
+
+    conn.raw(comando)
+        .then((data) => {
+        res.status(200).send({msg: "Cliente cadastrado com sucesso!"});
+    })
+        .catch((error) => {
+        res.status(500).send("Erro ao cadastrar um cliente!");
     });
+    }, 
+    
+    consultar: (req, res) => {
+        conn.raw("SELECT * FROM cliente")
+            .then((data) => {
+                res.status(200).send(data[0]); 
+            })
+            .catch((error) => {
+                console.error(error); 
+                res.status(500).send("Erro ao consultar os clientes!");
+            });
     },
-    atualizar: (req, res) => { },
-    deletar: (req, res) => { }
-})
+    atualizar: (req, res) => {
+        varcomando2 = ``;
+        conn.raw(`update cliente(nome, telefone, email) set values ('${nome}', '${telefone}, '${email})`)
+     },
+    deletar: (req, res) => { },
+});
 
 //OUTRO MÉTODO PARA FAZER CONSULTA USANDO ASYNC E AWAIT
 /*CONSULTAR: async (req,res) => {
